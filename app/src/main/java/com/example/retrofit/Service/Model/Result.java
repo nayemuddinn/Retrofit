d698fd6c366bb3b3b613d0786000fac4fbd56e81
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2023-06-09 21:11:27 +0600

    created retrofit singleton class

--- app/src/main/java/com/example/retrofit/Service/Model/Result.java
+++ app/src/main/java/com/example/retrofit/Service/Model/Result.java
@@ -1,4 +1,23 @@
 package com.example.retrofit.Service.Model;
 
+import java.util.List;
+
 public class Result {
+
+    private List<SportModel> Result;
+
+    public Result() {
+    }
+
+    public Result(List<SportModel> result) {
+        Result = result;
+    }
+
+    public List<SportModel> getResult() {
+        return Result;
+    }
+
+    public void setResult(List<SportModel> result) {
+        Result = result;
+    }
 }
--- app/src/main/java/com/example/retrofit/Service/Network/RetrofitInstance.java
+++ app/src/main/java/com/example/retrofit/Service/Network/RetrofitInstance.java
@@ -1,4 +1,17 @@
 package com.example.retrofit.Service.Network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
+
+    private static Retrofit retrofit;
+    private static String Base_url="https://cricket.sportmonks.com/api/v2.0/";
+
+    public static Retrofit getRetrofit()
+    {
+        if(retrofit==null)
+            retrofit=new Retrofit.Builder().baseUrl(Base_url).addConverterFactory(GsonConverterFactory.create()).build();
+        return retrofit;
+    }
 }
